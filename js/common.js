5e1acd7e77a1df21388d441647cc4f247dcb0855
harish-hyperDev <coolprogrammer.777@gmail.com>
2022-05-27 20:11:23 +0530

    working on some minor improvements

--- js/common.js
+++ js/common.js
@@ -1,8 +1,21 @@
 const height = 300;
 const width = 1000;
 
-dateRange = [new Date(2015, 0).getTime(), new Date(2020, 0).getTime()];
-xScale = d3.scaleTime().range([80, width - 20]).domain(dateRange);
+// dateRange = [new Date(2015, 0).getTime(), new Date(2020, 0).getTime()];
+// ranger = 
+// dateRange = (jsondata, d3.extent( d => d.InstallDate ) ) ;
+// d3.extent(jsondata, function(d) { d.InstallDate })
+// d3.extent(data.movies, function (d) { return d.movie_release_date; })
+// xScale = d3.scaleTime().range([80, width - 20]).domain(jsondata, function (d) { return d.InstallDate.getTime() });
+dataRange = [new Date(2022, 0).getTime(), new Date(2022, 12).getTime()]
+console.log(dataRange)
+xScale = d3.scaleTime().range([80, width]).domain(dataRange);
+
+/* 
+  var x = d3.scaleLinear()
+      .range([0, width - margin.left - margin.right]);
+      .domain(d3.extent(data.movies, function (d) { return d.movie_release_date.getYear()+1900; })); 
+*/
 
 const numberOfEvents = {
   title: 'Number of Events',
@@ -49,7 +62,6 @@ xAxis = svg => svg.append('g')
     .attr('transform', 'translate(0, -2)'));
 
 labels = svg => svg.append('g')
-
   .attr('transform', `translate(20, ${height / 2})`)
   .call(el => el.append('text')
     .text('PRO')
@@ -61,73 +73,84 @@ labels = svg => svg.append('g')
     .attr('transform', 'translate(0, 15)'));
 
 
-  const svg = d3.select('#container')
-    .append("svg")
-    .attr('width', width)
-    .attr('height', height)
-    .attr('font-family', 'sans-serif')
-    .call(xAxis)
-    .call(labels);
+const svg = d3.select('#container')
+  .append("svg")
+  .attr('width', width)
+  .attr('height', height)
+  .attr('font-family', 'sans-serif')
+  .call(xAxis)
+  .call(labels);
 
-
-  function update(data) {
-    svg.selectAll('.date-bin')
-      // .data(data, d => { console.log(d[0], d[0]) })
-      .data(data, d => d)
-      .join('g')
-      .attr('class', 'date-bin')
-      .attr('transform', d => `translate(${xScale(new Date(d.InstallDate))}, ${height / 2})`)
-      .selectAll('.date-bin-group')
-      .data(d => d[1], d => d[0])
-      .join('g')
-      .attr('class', 'date-bin-group')
-      .attr('transform', d => `translate(0, ${15 * (d[0] ? 1 : -1)})`)
-      .selectAll('.event')
-      .data(d => d[1], d => d.uid)
-      .join(
-        enter => enter.append('circle')
-          .attr('fill', d => d3.schemeTableau10[d.category])
-          .attr('r', 4)
-          .attr('cx', 0)
-          .attr('cy', 0)
-          .attr('class', 'event')
-          .attr('transform', (d, i, nodes) => `${console.log(d.position)}translate(0, ${i * 10 * (d.position ? 1 : -1)})`)
-          .call(el => el.attr('opacity', 0)
-            .transition().duration(200)
-            .attr('opacity', 1)),
-        update => update
-          .call(els => els.transition().duration(200)
-            .attr('transform', (d, i, nodes) => `translate(0, ${i * 10 * (d.position ? 1 : -1)})`)),
-        exit => exit.remove());
+function colorOnPatchCategory(category) {
+  switch (category) {
+    case "Definition Update": return 1;
+    case "Updates": return 2;
+    case "": return 2;
+    default: return 9;
   }
+}
+
+function update(data) {
+  svg.selectAll('.date-bin')
+    // .data(data, d => { console.log(d[0], d[0]) })
+    .data(data, d => d)
+    .join('g')
+    .attr('class', 'date-bin')
+    .attr('transform', d => `translate(${xScale(new Date(d.InstallDate))}, ${height / 2})`)
+    .selectAll('.date-bin-group')
+    .data(d => d)
+    .join('g')
+    .attr('class', 'date-bin-group')
+    .attr('transform', d => `translate(0, ${15 * (d.PatchCategory ? 1 : -1)})`)
+    // .selectAll('.event')
+    .data(d => d.PatchCategory/* , d => d.uid */)
+    .join(
+      enter => enter.append('circle')
+        .attr('fill', d => d3.schemeTableau10[colorOnPatchCategory(d.PatchCategory)])
+        .attr('r', 4)
+        .attr('cx', 0)
+        .attr('cy', 0)
+        // .attr('class', 'event')
+        // .attr('transform', (d, i, nodes) => `translate(0, ${i * 10 * (d.PatchCategory ? 1 : -1)})`)
+        .attr('transform', (d, i, nodes) => `translate(0, ${i * 10 * (d.PatchCategory ? 1 : -1)})`)
+        .call(el => el.attr('opacity', 1)
+          .transition().duration(1000)
+          .attr('opacity', 1))
+      // update => update
+      //   .call(els => els.transition().duration(200)
+      //     .attr('transform', (d, i, nodes) => `translate(0, ${i * 10 * (console.log(d.PatchCategory) ? 1 : -1)})`)),
+
+    )
+}
 
 update(jsondata)
 
 
 
 
-// events = () => {
-//   const events = new Array(numberOfEvents).fill(0);
-//   events.forEach((event, i) => {
-//     const randomTime = Math.random() * (dateRange[1] - dateRange[0]) + dateRange[0];
-//     events[i] = {
-//       uid: i,
-//       position: Math.round(Math.random()) === 1,
-//       category: Math.floor(Math.random() * numberOfCategories),
-//       date: new Date(randomTime)
-//     };
-//   });
+events = () => {
+  const events = new Array(numberOfEvents).fill(0);
+  console.log('this is events')
+  events.forEach((event, i) => {
+    const randomTime = Math.random() * (dateRange[1] - dateRange[0]) + dateRange[0];
+    events[i] = {
+      uid: i,
+      position: Math.round(Math.random()) === 1,
+      category: Math.floor(Math.random() * numberOfCategories),
+      date: new Date(randomTime)
+    };
+  });
 
-//   events.sort((a, b) => a.date.getTime() - b.date.getTime());
-//   return events;
-// }
+  events.sort((a, b) => a.date.getTime() - b.date.getTime());
+  return events;
+}
 
 // let ev = events()
 
-let groupedEvents = d3.group(
-  ev.filter(e => categories().includes(`${e.category}`)),
-  d => new Date(d.date.getFullYear(), d.date.getMonth()).toDateString(),
-  console.log(d => d.position),
-  d => d.position)
+// let groupedEvents = d3.group(
+//   ev.filter(e => categories().includes(`${e.category}`)),
+//   d => new Date(d.date.getFullYear(), d.date.getMonth()).toDateString(),
+//   console.log(d => d.position),
+//   d => d.position)
 
 
